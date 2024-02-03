eeb95611f23d2a8c8d28bcfa51422b2bbdb4ed81
phanuphong100stars <panuphong@100stars.com>
2024-02-03 18:10:10 +0700

    feat: init

---
+++ android/app/src/main/kotlin/com/example/discord_bot_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.discord_bot_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:discord_bot_manager/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const HomePage(),
+        });
+  }
+}
